1d110a3c1269c648a85def2ba864b1e557d8c015
Akksheetha <akkshee@gmail.com>
2026-06-02 08:03:22 +0530

    Recent Hands On Commit

---
+++ SetPractice/Duplicates.py
@@ -0,0 +1,5 @@
+def remove_duplicates(input_list):
+    return set(input_list)
+
+lst = [1, 2, 2, 3, 4, 4]
+print(remove_duplicates(lst))
--- SetPractice/UnionPractice.py
+++ SetPractice/UnionPractice.py
@@ -1,3 +1,5 @@
-set1 = set(input("Enter numbers :").split())
-set2 = set(input("Enter numbers :").split())
-print(set1|set2)
+def union_sets(set1, set2):
+    return set1 | set2
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+print(union_sets(s1, s2))
